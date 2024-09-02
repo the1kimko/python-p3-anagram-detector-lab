baf9683be5b6a3a2b05d323a59a016dd4485b350
KomfortKimko <komfortkimk@gmail.com>
2024-09-02 07:44:27 +0300

    Anagram-detector

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, words):
+        """Returns a list of words that are anagrams of the initialized word."""
+        # Normalize the word by sorting its letters
+        sorted_word = sorted(self.word)
+        # Find and return all matching anagrams from the input list
+        return [w for w in words if sorted(w) == sorted_word]
